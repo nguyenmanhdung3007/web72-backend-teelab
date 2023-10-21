4a2284ea144c9b72944dea58bf0b525e33627688
Cao Khánh Hưng <71182720+caohung131@users.noreply.github.com>
2023-10-21 21:59:34 +0700

    Cao hung cap nhat 21/10

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const userRouter = require('./users');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use("/", userRouter)
 
 module.exports = router;
